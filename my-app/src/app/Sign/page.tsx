8fe1b2e1d7f2b0274b54a898e87111a1d28cba2a
alireza rahimi <alireza.ir76@gmail.com>
2024-01-27 15:21:23 +0330

    make a simple Signup page

---
+++ my-app/src/app/Sign/page.tsx
@@ -0,0 +1,10 @@
+import SignUp from "@/components/Login/index";
+
+
+export default function Sgin() {
+  return (
+    <main>
+      <SignUp/>
+    </main>
+  );
+}
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Sign from "../app/Sign/page"
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main >
+        <Sign />
+    </main>
+  );
+}
